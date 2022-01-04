fa538ff2ae6c6c4582492efc11360d65df2347df
Divyanshi Goyal <divyanshigoyal16@gmail.com>
2022-01-05 02:54:27 +0530

    Add project files.

---
+++ API/Controllers/ComponentProcessingController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ComponentProcessingController : BaseApiController
+    {
+        private readonly IProcessRequestRepository _repo;
+        public ComponentProcessingController(IProcessRequestRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProcessRequest>> GetProcessRequest(int id){
+            return await _repo.GetProcessRequestByIdAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProcessResponse>> ProcessDetail([FromBody]ProcessRequest processRequest){
+            return await _repo.ProcessDetail(processRequest); //db save
+            //call p&d --> ct, q = charges 
+            // charges ++processResponse
+            //pr -->charges
+            // pr --> return
+        }
+        [HttpPost("completeprocessing")]
+        public ActionResult<string> CompleteProcessing(){
+            return  _repo.CompleteProcessing();
+        }
+
+    }
+}
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  
+  title = 'Return Order Management';
+  constructor(private accountService: AccountService) {}
+  
+  ngOnInit(): void {
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+    if(token){
+      this.accountService.loadCurrentUser(token).subscribe(() =>{
+        console.log('user loaded');
+      }, error =>{
+        console.log(error);
+      });
+
+    }
+  }
+}
+  
